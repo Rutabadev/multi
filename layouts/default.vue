bfc0790320aec4d8f0705ed6675ded3c58718478
etienne.raby <etienne.raby@fittingbox.com>
2020-02-12 15:10:18 +0100

    wagadoudou in navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -81,6 +81,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-motorbike',
+          title: 'Wagadou',
+          to: '/wagadoudou'
         }
       ],
       miniVariant: false,
