ee5d0d57015e612e06ad7539eca43f13b02bf0a1
etienne.raby <etienne.raby@fittingbox.com>
2020-02-12 17:04:34 +0100

    continued form

--- components/Form.vue
+++ components/Form.vue
@@ -3,33 +3,35 @@
     <v-form v-model="valid">
       <v-container>
         <v-row>
-          <v-col cols="12" md="4">
+          <v-col cols="12" md="6">
             <v-text-field
-              v-model="firstname"
-              :rules="nameRules"
+              v-model="username"
+              :rules="usernameRules"
               :counter="10"
-              label="First name"
+              label="Username"
               required
             ></v-text-field>
           </v-col>
 
-          <v-col cols="12" md="4">
+          <v-col cols="12" md="6">
             <v-text-field
-              v-model="lastname"
-              :rules="nameRules"
-              :counter="10"
-              label="Last name"
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
               required
             ></v-text-field>
           </v-col>
-
-          <v-col cols="12" md="4">
-            <v-text-field
-              v-model="email"
-              :rules="emailRules"
-              label="E-mail"
-              required
-            ></v-text-field>
+        </v-row>
+        <v-row align="center">
+          <v-col class="text-center" cols="12" md="12">
+            <v-btn :disabled="!valid" @click="validate" color="primary" rounded
+              >Submit</v-btn
+            >
+          </v-col>
+        </v-row>
+        <v-row v-if="success" align="center">
+          <v-col class="text-center" cols="12" md="12">
+            Success!
           </v-col>
         </v-row>
       </v-container>
@@ -40,19 +42,25 @@
 <script>
 export default {
   data: () => ({
+    success: false,
     valid: false,
-    firstname: '',
-    lastname: '',
-    nameRules: [
-      (v) => !!v || 'Name is required',
-      (v) => v.length <= 10 || 'Name must be less than 10 characters',
-      (v) => v.length > 3 || 'Name must be less more 3 characters'
+    username: '',
+    usernameRules: [
+      (v) => !!v || 'Username is required',
+      (v) => v.length <= 10 || 'Username must be less than 10 characters',
+      (v) => v.length > 3 || 'Username must be less more 3 characters'
     ],
-    email: '',
-    emailRules: [
-      (v) => !!v || 'E-mail is required',
-      (v) => /.+@.+/.test(v) || 'E-mail must be valid'
+    password: '',
+    passwordRules: [
+      (v) => !!v || 'Password is required',
+      (v) => /.+\..+@.+\..+/.test(v) || 'Password must be valid'
     ]
-  })
+  }),
+
+  methods: {
+    validate() {
+      this.success = true
+    }
+  }
 }
 </script>
