0b795c68582f7f1d6211dee8042787fcce280cb2
etienne.raby <etienne.raby@fittingbox.com>
2020-02-14 16:59:38 +0100

    cleaned default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    ></v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn @click.stop="miniVariant = !miniVariant" icon>
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn @click.stop="clipped = !clipped" icon>
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn @click.stop="fixed = !fixed" icon>
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+    <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn @click="$vuetify.theme.dark = !$vuetify.theme.dark" icon>
@@ -49,9 +32,6 @@
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
         {
           icon: 'mdi-apps',
@@ -69,9 +49,6 @@ export default {
           to: '/about'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'Vuetify.js'
     }
   }
